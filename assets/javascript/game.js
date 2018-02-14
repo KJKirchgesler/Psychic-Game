23dba43fc8c3e829f86967265dcb1365da0fb048
KRISTEN KIRCHGESLER <kristen@KRISTENs-MacBook-Pro.local>
2018-02-14 18:06:30 -0500

    uploaded updated code in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+// Creates an array that lists out all of the letters of the alphabet.
+var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u" "v", "w", "x", "y", "z"];
+
+// Variables of wins, losses, and guesses left. Wins and losses start at 0.  Guesses start at 9.
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+
+  // Determines which key was pressed.
+  var userGuess = event.key;
+
+  // Randomly chooses a choice from the options array. This is the Computer's guess.
+  var computerSelects = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+
+
+  // This determines wins, losses and remaining guesses.
+  if (userGuess === computerSelects){
+    wins++;
+    } else {
+    guesses--;
+    }
+
+if (guesses === 0){
+    losses++
+}
+
+
+    // Creates variable for HTML to keep track of wins, losses and guesses left.
+    var html =
+      "<p>Wins: " + wins + "</p>" +
+      "<p>Losses: " + losses + "</p>" +
+      "<p>Guesses: " + guesses + "</p>";
+
+    // Set the inner HTML contents of the #game div to our html string
+    document.querySelector("#game").innerHTML = html;
+  }
+};
