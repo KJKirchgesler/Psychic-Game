c82821fd0f78239636afe7bcd48433817b8f284b
KRISTEN KIRCHGESLER <kristen@KRISTENs-MacBook-Pro.local>
2018-02-15 10:18:53 -0500

    fixed linking between index.html and game.js and fixed bugs related to mismatched guessesLeft variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 // Creates an array that lists out all of the letters of the alphabet.
-var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u" "v", "w", "x", "y", "z"];
+var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Variables of wins, losses, and guesses left. Wins and losses start at 0.  Guesses start at 9.
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 
+
 // This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
   // Determines which key was pressed.
   var userGuess = event.key;
@@ -17,24 +18,21 @@ document.onkeyup = function(event) {
 
 
   // This determines wins, losses and remaining guesses.
-  if (userGuess === computerSelects){
+  if (userGuess === computerSelects) {
     wins++;
     } else {
-    guesses--;
+    guessesLeft--;
     }
-
-if (guesses === 0){
+  if (guessesLeft === 0) {
     losses++
-}
-
-
-    // Creates variable for HTML to keep track of wins, losses and guesses left.
-    var html =
-      "<p>Wins: " + wins + "</p>" +
-      "<p>Losses: " + losses + "</p>" +
-      "<p>Guesses: " + guesses + "</p>";
-
-    // Set the inner HTML contents of the #game div to our html string
-    document.querySelector("#game").innerHTML = html;
   }
-};
+
+  // Creates variable for HTML to keep track of wins, losses and guesses left.
+  var html =
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>"; +
+    "<p>Guesses: " + guessesLeft + "</p>";
+
+  // Set the inner HTML contents of the #game div to our html string
+  document.querySelector("#game").innerHTML = html;
+}
