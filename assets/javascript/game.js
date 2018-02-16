4650211e3ec7398fad4c96875daad8236011a55b
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-02-15 22:40:17 -0500

    Fixed bugs in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var lettersGuessed = [];
 
 
 // This function is run whenever the user presses a key.
@@ -13,26 +14,30 @@ document.onkeyup = function (event) {
   // Determines which key was pressed.
   var userGuess = event.key;
 
+  //Populates the array for Guesses so far:
+  lettersGuessed.push(userGuess);
+
   // Randomly chooses a choice from the options array. This is the Computer's guess.
   var computerSelects = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
 
-  // This determines wins, losses and remaining guesses.
+  // This determines wins, losses, remaining guesses and resets guesses so far.
   if (userGuess === computerSelects) {
     wins++;
-    } else {
+    guessesLeft = 9;
+    lettersGuessed = [];
+  } else {
     guessesLeft--;
-    }
+  }
   if (guessesLeft === 0) {
-    losses++
+    losses++;
+    guessesLeft = 9;
+    lettersGuessed = [];
   }
 
-  // Creates variable for HTML to keep track of wins, losses and guesses left.
-  var html =
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>"; +
-    "<p>Guesses: " + guessesLeft + "</p>";
-
-  // Set the inner HTML contents of the #game div to our html string
-  document.querySelector("#game").innerHTML = html;
+  //Embeds the tallies of wins, losses, guesses left and guesses so far into html file.
+  document.getElementById('wins').innerHTML = + wins;
+  document.getElementById('losses').innerHTML = + losses;
+  document.getElementById('guessesLeft').innerHTML = + guessesLeft;
+  document.getElementById('lettersGuessed').innerHTML = lettersGuessed.join(' , ');
 }
